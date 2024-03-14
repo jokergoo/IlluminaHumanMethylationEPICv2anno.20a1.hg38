f4d50dfa4b86c383810ca0b3c8a68ffb88da62a2
Zuguang Gu <jokergoo@gmail.com>
2024-03-14 13:17:20 +0100

    update

--- R/zzz.R
+++ R/zzz.R
@@ -26,3 +26,47 @@
 #         namespaceExport(ns, var)
 #     }
 # }
+
+
+# == title
+# Aggregate to the probe-level
+#
+# == param
+# -x A data frame or a matrix.
+#
+# == details
+# If ``x`` is a data frame, it should be one of the following objects: Islands.UCSC, Locations, Other, SNPs.Illumina, SNPs.141CommonSingle, SNPs.142CommonSingle, SNPs.144CommonSingle, 
+#  SNPs.146CommonSingle, SNPs.147CommonSingle, SNPs.150CommonSingle, SNPs.151CommonSingle.
+#
+# If ``x`` is a matrix, it should come from the analysis with the **minfi** package. If multiple rows correspond to the same
+# probe, the average value of rows is simply used.
+aggregate_to_probes = function(x) {
+	nm = as.character(substitute(x))
+
+	if(grepl("(Islands.UCSC|Locations|Other|SNPs.Illumina|SNPs.141CommonSingle|SNPs.142CommonSingle|SNPs.144CommonSingle|SNPs.146CommonSingle|SNPs.147CommonSingle|SNPs.150CommonSingle|SNPs.151CommonSingle)$", nm)) {
+		probe_ID = gsub("_.*$", "", rownames(x))
+		x = x[!duplicated(probe_ID), , drop = FALSE]
+		rownames(x) = gsub("_.*$", "", rownames(x))
+		x
+	} else if(grepl("Manifest$", nm)) {
+		stop("You cannot aggregate to the probes with the `Manifest` object.")
+	} else {
+		if(all(apply(x, 2, is.numeric))) {
+			x = as.matrix(x)
+			rn = unique(gsub("_.*$", "", rownames(x)))
+			x2 = do.call(rbind, tapply(1:nrow(x), gsub("_.*$", "", rownames(x)), function(ind) {
+			    colMeans(x[ind, , drop = FALSE], na.rm = TRUE)
+			}, simplify = FALSE))
+			x2[rn, , drop = FALSE]
+		} else {
+			if(!is.null(rownames(x))) {
+				probe_ID = gsub("_.*$", "", rownames(x))
+				x = x[!duplicated(probe_ID), , drop = FALSE]
+				rownames(x) = gsub("_.*$", "", rownames(x))
+				x
+			} else {
+				stop("Wrong format")
+			}
+		}
+	}
+}
--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@ IlluminaHumanMethylationEPICv2anno.20a1.hg38
 This package provides annotations for Illumina methylation EPIC array v2.0. The data is based on the file https://support.illumina.com/content/dam/illumina-support/documents/downloads/productfiles/methylationepic/MethylationEPIC%20v2%20Files.zip from 
   https://support.illumina.com/array/array_kits/infinium-methylationepic-beadchip-kit/downloads.html.
 
-When using with the **minfi** package, you can manually set the "annotation" element by providing the surfix (removing the string "IlluminaHumanMethylationEPICv2anno."):
+When using with the **minfi** package, you can manually set the "annotation" element by providing the suffix (removing the string "IlluminaHumanMethylationEPICv2anno."):
 
 ```r
 RGset = read.metharray.exp(...)
